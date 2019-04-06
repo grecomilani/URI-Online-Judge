6a21b5e46a32c8ed5647411d84981b5e43112b25
grecomilani <grecomilani@oc5355635755.ibm.com>
2019-04-06 14:14:55 -0300

    1001,1002,1003

---
+++ src/P1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P1001 {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		
+		int x = a + b;
+		System.out.println("X = " + x);
+		
+		scn.close();
+	}
+
+}
---
+++ src/P1003.java
@@ -0,0 +1,9 @@
+
+public class P1003 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
