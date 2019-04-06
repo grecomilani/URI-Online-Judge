e2447b46ffb528f699de948eca740337f5c91b84
grecolima <greco_lima@hotmail.com>
2019-04-06 15:32:39 -0300

    p1008

--- src/P1007.java
+++ src/P1007.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class P1007 {
 
 	public static void main(String[] args) {
 		
+		Scanner scn = new Scanner(System.in);
 		
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		int c = scn.nextInt();
+		int d = scn.nextInt();
+		
+		int diferenca = ((a * b) - (c * d));
+		
+		System.out.println("DIFERENCA = " + diferenca);
+		
+		scn.close();
 
 	}
 
---
+++ src/P1008.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class P1008 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scn = new Scanner(System.in);
+	    int a = scn.nextInt();
+	    int b = scn.nextInt();
+	    float c = scn.nextFloat();
+		
+	    System.out.printf("NUMBER = %d\n", a);
+	    System.out.printf("SALARY = U$ %.2f\n", b * c);
+		
+		scn.close();
+	}
+
+}
