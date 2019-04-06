3e43ec37230581589b86bf0be40a91eac6d63200
Grecomilani <grecomilani@oc5355635755.ibm.com>
2019-04-06 14:27:09 -0300

    1003,1004

--- src/P1004.java
+++ src/P1004.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class P1003 {
+public class P1004 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scn = new Scanner(System.in);
 		
 		int a = scn.nextInt();
 		int b = scn.nextInt();
 		
-		int soma = a + b;
+		int prod = a * b;
 		
-		System.out.println("SOMA = " + soma);
+		System.out.println("PROD = " + prod);
+		
 		scn.close();
-
 	}
 
 }
