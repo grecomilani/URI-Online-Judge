6a5a8d01a3714b2e524c0b1599dc43d637c88363
grecolima <greco_lima@hotmail.com>
2019-04-06 15:58:31 -0300

    p1009

---
+++ src/P1009.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class P1009 {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner scn = new Scanner(System.in);
+		
+		String nome = scn.next();
+		double salario = scn.nextDouble();
+		double totalVenda = scn.nextDouble();
+		
+		double x = salario + (totalVenda * 0.15);
+		System.out.printf("TOTAL = R$ %.2f\n",x);
+		
+		scn.close();
+
+	}
+
+}
