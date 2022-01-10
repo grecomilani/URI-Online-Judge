f8395bb358b66b2613830f435ec3114db7896083
GrecoLima <greco_lima@hotmail.com>
2022-01-10 18:57:16 -0300

    p1096

---
+++ src/P1096.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class P1096 {
+    public static void main(String[] args) throws IOException {
+        int i = 1;
+        while (i <= 9) {
+            for (int j = 7; j > 4; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+            i += 2;
+        }
+    }
+}
